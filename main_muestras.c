716a58bf58e7136b13d281e9f304eb079e76c25c
vargas-04 <75094430+vargas-04@users.noreply.github.com>
2020-11-27 19:47:16 -0600

    Create main_muestras.c

---
+++ main_muestras.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "muestras.h"
+int main(){
+
+    Muestras *arrMuestras;
+    int indice;
+    arrMuestras=(Muestras *)malloc(N*sizeof(Muestras));
+    llenarMuestras(arrMuestras);
+
+ return 0;
+}
