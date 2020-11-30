4ceff726380c5b77134b3568dcd739f3d926372b
iramvfranco <74212458+iramvfranco@users.noreply.github.com>
2020-11-30 01:20:57 -0600

    Update MuestrasEntradaCL.h

--- MuestrasEntradaCL.h
+++ MuestrasEntradaCL.h
@@ -2,14 +2,20 @@
 #define MUESTRASENTRADACL_H_INCLUDED
 #define MAX 30
 
+extern char muestraS [6][30];
+
 typedef struct{
    int *clave;
    char *nombre;
    char *departamento;
 } Muestras;
 
-Muestras *crearMuestras();
-void CapturarMuestras(Muestras *muestra);
-void ListarMuestras(Muestras muestra);
+Muestras *CrearMuestras(Muestras muestra);
+
+
+void crearMuestras(Muestras *muestras);
+void listarMuestras(Muestras muestras);
+void llenarMuestras(Muestras *arrMuestras);
+
 
 #endif // MUESTRASENTRADACL_H_INCLUDED
